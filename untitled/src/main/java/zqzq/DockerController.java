e6579beb86e8f8c8762aa87765884481792af245
zqIsNotAvailable <112932152+zqIsNotAvailable@users.noreply.github.com>
2022-11-04 17:05:15 +0800

    Add files via upload

---
+++ untitled/src/main/java/zqzq/DockerController.java
@@ -0,0 +1,12 @@
+package zqzq;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Docker!";
+    }
+}
